8e416859fcfab9f186d70f1247eae48d57afad5c
SATYASAIPANIGRAHI03 <SATYASAIPANIGRAHI03@GMAIL.COM>
2025-02-13 12:03:42 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AzureSqlMvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AzureSqlMvcApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureSqlMvcApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        
+        public decimal Price { get; set; }
+    }
+}
